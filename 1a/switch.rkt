f209e3000321bf601d454116945d901d08dce8fa
luludada <mingyangye801@gmail.com>
2018-07-18 15:13:12 +0800

    1a practice questions
    CPSC 1a practice questions completed

---
+++ 1a/switch.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Untitled) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
+(require 2htdp/image)
+s
+(overlay (rotate 30 (triangle 50 "solid" (color 255 0 0 255)))
+         (circle 80 "solid" "yellow")
+         (circle 100 "solid" "red"))
